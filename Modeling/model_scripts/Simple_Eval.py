500775d68b7578ec957e0fef1d345ab26e249bd8
Ryan Johnson <rjohnson18@ua.edu>
2024-06-25 16:41:08 -0400

    Build ML workflow using MLP model in PyTorch

---
+++ Modeling/model_scripts/Simple_Eval.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_percentage_error 
+import hydroeval as he
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.dates import MonthLocator, DateFormatter
+
+def Parity_Evaluation_Plots(DF, regionlist, modelname):
+
+    fontsize = 12
+    pred_col = f"{modelname}_swe_cm"
+
+# Subplots.
+    fig, ax = plt.subplots(1,1, 
+                           figsize=(8, 4))
+    plt.subplots_adjust(
+                    wspace=0.4
+                   )
+    fig.patch.set_facecolor('white')
+
+    #set min/max for y-axis of the predictions/observations
+    ymin = min(DF['ASO_swe_cm'])*1.1
+    ymax = max(DF['ASO_swe_cm'])*1.1
+    
+    #add color options
+    colors = ['blue', 'orange', 'red','green']
+
+    # Addscatter plot
+    for region in np.arange(0, len(regionlist),1):
+        regiondf = DF[DF['region']==regionlist[region]]
+        ax.scatter(regiondf['ASO_swe_cm'], regiondf[pred_col],
+                   c=colors[region], alpha=0.35, label=regionlist[region])
+
+     # Add some parameters.
+    ax.set_title('SWE Predictions', fontsize=fontsize)
+    ax.set_xlabel('Observations (cm)', fontsize=fontsize-2)
+    ax.set_ylabel('Predictions (cm)', fontsize=fontsize-2,)
+    ax.set_ylim(ymin, ymax)
+    ax.set_xlim(ymin, ymax)
+    ax.legend(fontsize=fontsize-2, loc='upper right')
+    
+    #Add a 1:1 prediction:observation plot
+    ax.plot((0,ymax),(0,ymax), linestyle = '--', color  = 'red')
+
+    plt.show()
+    
+    
+def TimeSeries_Evaluation_Plots(DF, predictions):
+
+# Subplots.
+    fig, ax = plt.subplots(1,1, figsize=(8, 7))
+    fig.patch.set_facecolor('white')
+
+    #set min/max for y-axis of the predictions/observations
+    ymin = min(DF['ASO_swe_cm'])*1.1
+    ymax = max(DF['ASO_swe_cm'])*1.1
+    
+    #add color options
+    colors = ['blue', 'red','green']
+    
+    ax.plot(DF['DOY'], DF['ASO_swe_cm'],
+                   c='orange', alpha=0.35, label= 'Observed')
+
+    # Add predictions to plot
+    for pred in np.arange(0, len(predictions),1):
+        ax.plot(DF['DOY'], DF[predictions[pred]],
+                   c=colors[pred], alpha=0.35, label=predictions[pred])
+
+     # Add some parameters.
+    ax.set_title('Streamflow Predictions', fontsize=16)
+    ax.set_xlabel('Time (DOY)', fontsize=14)
+    ax.set_ylabel('Streamflow (cfs)', fontsize=14,)
+    ax.set_ylim(0, ymax)
+    ax.legend(fontsize=14, loc='upper right')
+
+    plt.show()
+    
+
+#Define some key model performance metics: RMSE, PBias, MAE, MAPE
+def RMSE(DF, predictions):
+    eval_dict ={}
+    for pred in predictions:
+        rmse = mean_squared_error(DF['ASO_swe_cm'], DF[pred], squared=False)
+        #print('RMSE for ', predictions[pred], ' is ', rmse, ' cfs')
+        eval_dict[f"{pred}_rmse"] = rmse
+
+    eval = pd.DataFrame.from_dict(eval_dict, orient = 'index').T
+    return eval
+
+def MAPE(DF, predictions):
+    eval_dict ={}
+    for pred in predictions:
+        mape = round(mean_absolute_percentage_error(DF['ASO_swe_cm'], DF[pred])*100, 2)
+        #print('Mean Absolute Percentage Error for ', predictions[pred], ' is ', mape, '%')
+        eval_dict[f"{pred}_mape"] = mape
+    eval = pd.DataFrame.from_dict(eval_dict, orient = 'index').T
+    return eval
+        
+def PBias(DF, predictions):
+    eval_dict ={}
+    for pred in predictions:
+        pbias = he.evaluator(he.pbias,  DF[pred], DF['ASO_swe_cm'])
+        pbias = round(pbias[0],2)
+        #print('Percentage Bias for ', predictions[pred], ' is ', pbias, '%')
+        eval_dict[f"{pred}_pbias"] = pbias
+    eval = pd.DataFrame.from_dict(eval_dict, orient = 'index').T
+    return eval
+
+def KGE(DF, predictions):
+    eval_dict ={}
+    for pred in predictions:
+        kge, r, alpha, beta = he.evaluator(he.kge,  DF[pred], DF['ASO_swe_cm'])
+        kge = round(kge[0],2)
+        #print('Kling-Glutz Efficiency for ', predictions[pred], ' is ', kge)
+        eval_dict[f"{pred}_kge"] = kge
+    
+    eval = pd.DataFrame.from_dict(eval_dict, orient = 'index').T
+    return eval
+
+def Key_Stats(DF, predictions):  
+    eval_dict = {}
+    eval_dict['min_storage'] = min(DF['storage'])
+    eval_dict['max_storage'] = max(DF['storage'])
+    eval_dict['min_swe'] = min(DF['swe'])
+    eval_dict['max_swe'] = max(DF['swe'])
+    eval_dict['min_obs_flow'] = min(DF['ASO_swe_cm'])
+    eval_dict['max_obs_flow'] = max(DF['ASO_swe_cm'])
+    for pred in predictions:
+        eval_dict[f"{pred}_min"] = min(DF[pred])
+        eval_dict[f"{pred}_fmax"] = max(DF[pred])
+
+    eval = pd.DataFrame.from_dict(eval_dict, orient = 'index').T
+    return eval
+
+#function for randomly sampling predictions
+def SamplePreds(regionlist, PredsDF, df,n_samples):
+    print(n_samples)
+    PredsDF.insert(0, 'cell_id', df['cell_id'])
+    PredsDF.insert(1, 'Date', df['Date'])
+    PredsDF.insert(2, 'region', df['region'])
+
+    sampleDF = pd.DataFrame()
+    for region in regionlist:
+        DF = PredsDF[PredsDF['region']==region].copy()
+        DF = DF.sample(n=n_samples, random_state=69)
+
+        sampleDF = pd.concat([sampleDF, DF])
+
+    return sampleDF, PredsDF
+
+def Simple_Eval(regionlist, PredsDF, prediction_columns, modelname, plots = False, keystats = False):
+    
+    plotdf = PredsDF.copy()
+    plotdf.reset_index(inplace = True, drop = False)
+    Parity_Evaluation_Plots(plotdf, regionlist, modelname)
+
+    #put the below into a DF so we can compare all sites..determine overall and regional skill
+    region = pd.DataFrame()
+    region['region'] = ['Overall']
+
+    #Get RMSE from the model
+    rmse = round(RMSE(PredsDF, prediction_columns))
+
+    #Get Mean Absolute Percentage Error from the model
+    mape = round(MAPE(PredsDF, prediction_columns),2)
+
+    #Get Percent Bias from the model
+    pbias = round(PBias(PredsDF, prediction_columns), 2)
+
+    #Get Kling-Gutz Efficiency from the model
+    kge = round(KGE(PredsDF, prediction_columns),2)
+
+    EvalDF = pd.DataFrame()
+    EvalDF = pd.concat([EvalDF, region, kge,rmse,mape,pbias],axis = 1)
+
+
+    for region in regionlist:
+        regionDF = PredsDF[PredsDF['region']==region].copy()
+        reg = pd.DataFrame()
+        reg['region'] = [region]
+
+        #Get RMSE from the model
+        rmse = round(RMSE(regionDF, prediction_columns))
+
+        #Get Mean Absolute Percentage Error from the model
+        mape = round(MAPE(regionDF, prediction_columns),2)
+
+        #Get Percent Bias from the model
+        pbias = round(PBias(regionDF, prediction_columns), 2)
+
+        #Get Kling-Gutz Efficiency from the model
+        kge = round(KGE(regionDF, prediction_columns),2)
+
+        evaldf = pd.DataFrame()
+        evaldf = pd.concat([evaldf, reg, kge,rmse,mape,pbias],axis = 1)
+        EvalDF = pd.concat([EvalDF, evaldf])
+
+
+    display(EvalDF)
+
+    return EvalDF
+        
+
---
+++ Modeling/model_scripts/mlp_dataprocessing.py
@@ -0,0 +1,219 @@
+# hydrological packages
+import hydroeval as he
+from hydrotools.nwm_client import utils 
+
+# basic packages
+import numpy as np
+import pandas as pd
+import os
+import pyarrow as pa
+import pyarrow.parquet as pq
+#import bz2file as bz2
+
+# system packages
+#from progressbar import ProgressBar
+from datetime import datetime, date
+import datetime
+import pickle as pkl
+import warnings
+warnings.filterwarnings("ignore")
+import platform
+import time
+
+# data analysi packages
+from scipy import optimize
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RepeatedKFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+import joblib
+
+# deep learning packages
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+
+#Shared/Utility scripts
+import sys
+import boto3
+import s3fs
+#sys.path.insert(0, '../..') #sys allows for the .ipynb file to connect to the shared folder files
+from model_scripts import Simple_Eval
+
+#load access key
+HOME = os.path.expanduser('~')
+KEYPATH = "SWEMLv2.0/AWSaccessKeys.csv"
+ACCESS = pd.read_csv(f"{HOME}/{KEYPATH}")
+
+#start session
+SESSION = boto3.Session(
+    aws_access_key_id=ACCESS['Access key ID'][0],
+    aws_secret_access_key=ACCESS['Secret access key'][0],
+)
+S3 = SESSION.resource('s3')
+#AWS BUCKET information
+BUCKET_NAME = 'national-snow-model'
+BUCKET = S3.Bucket(BUCKET_NAME)
+
+#s3fs
+fs = s3fs.S3FileSystem(anon=False, key=ACCESS['Access key ID'][0], secret=ACCESS['Secret access key'][0])
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Device:", device)
+
+def data_clean(df, regionlist): #add region to dataframe
+    #clean data. For example, approximately 420,000 locations/timesteps in the Southern rockies domain show VIIRS as having snow but show 0" SWE. Likely an error and remove these sites.
+    bad_low_ASO_data = df[(df['swe_cm']< 0.1) & (df['hasSnow'] == True)] #using VIIRS obs to "clean the data"
+    bad_VIIRS_data = df[(df['swe_cm']> 0.1) & (df['hasSnow'] == False)]
+
+    print(f"The provided data contains {len(df)} data points, of which {len(bad_low_ASO_data)} locations/timesteps show no SWE and VIIRS fsca > 20%")
+    print(f"{len(bad_VIIRS_data)} locations/timesteps show SWE and VIIRS fsca < 20%")
+
+    #remove unrealistically high values, values higher than below are most likely bad lidar obs
+    bad_high_ASO_data = pd.DataFrame()
+    for region in regionlist:
+        if region == 'Southwest':
+            value = 400
+            bad_df = df[(df['region'] == region) & (df['swe_cm'] > value)] # This is ~150" of SWE
+            print(f"{len(bad_high_ASO_data)} locations/timesteps show SWE greater than a realistic value ({value} cm) in the {region} domain")
+        if region == 'Northwest':
+            value = 800
+            bad_df = df[(df['region'] == region) & (df['swe_cm'] > 800)] # This is ~300" of SWE
+            print(f"{len(bad_high_ASO_data)} locations/timesteps show SWE greater than a realistic value ({value} cm) in the {region} domain")
+        if region == 'SouthernRockies':
+            value = 250
+            bad_df = df[(df['region'] == region) & (df['swe_cm'] > 250)] # This is ~100" of SWE
+            print(f"{len(bad_high_ASO_data)} locations/timesteps show SWE greater than a realistic value ({value} cm) in the {region} domain")
+        
+        bad_high_ASO_data = pd.concat([bad_high_ASO_data, bad_df])
+
+
+
+
+    print('removing..')
+
+    dfclean = pd.concat([df,bad_high_ASO_data, bad_low_ASO_data, bad_VIIRS_data])
+    dfclean = dfclean[~dfclean.index.duplicated(keep=False)]
+    dfclean.reset_index(inplace=True, drop=True)
+
+    print(f"There are {len(dfclean)} datapoints for model training/testing.")
+
+    return dfclean
+
+
+def mlp_scaler(regionlist, df, years, splitratio, test_years, target, input_columns, model_path, scalertype = 'MinMax'):
+    #check to make sure the model path exists
+    if not os.path.exists(model_path):
+        os.makedirs(model_path, exist_ok=True)
+
+    # #Select training data
+    # if years == True:
+    #     #training years
+    #     x_train = df[~df.datetime.dt.year.isin(test_years)]
+    #     x_train.pop('Date')
+    #     y_train = x_train[target]
+    #     x_train.pop(target)
+    #     x_train = x_train[input_columns]
+
+    #     #testing years
+    #     x_test = df[df.datetime.dt.year.isin(test_years)]
+    #     x_test.pop('Date')
+    #     y_test = x_test[target]
+    #     x_test.pop(target)
+    #     x_test = x_test[input_columns]
+
+    #     #Convert dataframe to numpy, scale, save scalers
+    #     y_train_np = y_train.to_numpy()
+    #     x_train_np = x_train.to_numpy()
+    #     x_test_np = x_test.to_numpy()
+        
+    # else:
+    #take random sample of each region to ensure they are in the testing data
+    x_train, y_train, x_test, y_test = pd.DataFrame(), pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
+    for region in regionlist:
+        regionDF = df[df['region'] == region].copy()
+
+        display(regionDF.head(5))
+
+        X = regionDF[input_columns]
+        y = regionDF[target]
+
+        x_train_reg, x_test_reg, y_train_reg, y_test_reg = train_test_split(X, y, test_size=splitratio, random_state=69)
+
+        x_train = pd.concat([x_train, x_train_reg])
+        x_test = pd.concat([x_test, x_test_reg])
+        y_train = pd.concat([y_train, y_train_reg])
+        y_test = pd.concat([y_test, y_test_reg])
+
+
+
+    #convert to numpy
+    y_train_np = y_train.to_numpy()
+    x_train_np = x_train.to_numpy()
+    x_test_np = x_test.to_numpy()
+
+    scalerfilepath_x = f"{model_path}/scaler_x.save"
+    scalerfilepath_y = f"{model_path}/scaler_y.save"
+
+    if scalertype == 'MinMax':
+        scaler = MinMaxScaler() #potentially change scalling...StandardScaler
+        x_train_scaled = scaler.fit_transform(x_train_np)
+        x_test_scaled = scaler.fit_transform(x_test_np)
+        joblib.dump(scaler, scalerfilepath_x)
+
+        scaler = MinMaxScaler() #potentially change scalling...StandardScaler
+        y_scaled_train = scaler.fit_transform(y_train_np.reshape(-1, 1))
+        joblib.dump(scaler, scalerfilepath_y)  
+
+    if scalertype == 'Standard':
+        scaler = StandardScaler() #potentially change scalling...StandardScaler
+        x_train_scaled = scaler.fit_transform(x_train_np)
+        joblib.dump(scaler, scalerfilepath_x)
+
+        scaler = StandardScaler() #potentially change scalling...StandardScaler
+        y_scaled_train = scaler.fit_transform(y_train.reshape(-1, 1))
+        joblib.dump(scaler, scalerfilepath_y) 
+
+    print(f"y train shape {y_scaled_train.shape}")
+    print(f"x train shape {x_train_scaled.shape}")
+    print(f"x test shape {x_test_scaled.shape}")
+
+    return x_train_scaled, y_scaled_train, x_test_scaled, x_test, y_test
+
+# def mlp_testscaler(df, years, splitratio, test_years, target, input_columns, model_path):
+#     #Get water year for testing from larger dataset
+#     x_test_temp = df[df.datetime.dt.year.isin(test_years)]
+#     x_test_temp_1 = x_test_temp.copy()
+#     station_index_list = x_test_temp_1['station_id']
+#     x_test_temp_1.pop('station_id')
+#     x_test_temp_1.pop('datetime')
+
+#     #Get target variable (y) and convert to numpy arrays
+#     y_test_temp_1 = x_test_temp_1[target]
+#     x_test_temp_1.pop(target)
+#     x_test_temp_1 = x_test_temp_1[input_columns]
+#     x_test_1_np = x_test_temp_1.reset_index(drop=True).to_numpy()
+#     #y_test_1_np = y_test_temp_1.reset_index(drop=True).to_numpy()
+
+
+#     #load scalers and scale
+#     scalerfilepath_x = f"{model_path}/scaler_x.save"
+
+#     #load scalers
+#     scaler_x = joblib.load(scalerfilepath_x)
+#     # scaler_y = joblib.load(scalerfilepath_y)
+
+#     #scale the testing data
+#     x_test_1_scaled = scaler_x.fit_transform(x_test_1_np)
+#     #y_scaled_test_1 = scaler_y.fit_transform(y_test_1_np.reshape(-1, 1))
+#     #print(y_scaled_test_1.shape)
+#     print(x_test_1_scaled.shape)
+
+#     return x_test_1_scaled, y_test_temp_1, x_test_temp, station_index_list
